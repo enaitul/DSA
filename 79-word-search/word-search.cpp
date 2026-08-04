df530b07b2d1720264a602c2452ad61d6ace272e
Md Enaitul Hoque <127648815+enaitul@users.noreply.github.com>
2026-08-04 20:21:31 +0530

    Time: 845 ms (15.72%) | Memory: 11.6 MB (29.06%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool dfs (vector<vector<char>>& board, string word, int row, int col, int index){
+        if (index == word.size()){
+            return true;
+        }
+        
+        if (row<0 || col<0 || row>= board.size() || col>= board[0].size() || board[row][col] !=  word[index]) return false;
+
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        bool found = dfs(board, word, row + 1, col, index + 1) || dfs(board, word, row - 1, col, index + 1) || dfs(board, word, row, col + 1, index + 1) || dfs(board, word, row, col - 1, index + 1);
+
+        board[row][col] = temp;
+
+        return found;
+
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i<n; i++){
+            for (int j = 0; j<m; j++){
+                if (dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+};
