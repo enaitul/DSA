c5e2abf8cc966e19eadc14d33cc9b9c7648c598c
Md Enaitul Hoque <127648815+enaitul@users.noreply.github.com>
2026-08-07 17:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (65.08%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve (int index, vector<int>& nums, vector<int> &dp){
+        if (index < 0) return 0;
+        if (index == 0) return nums[0];
+        if (dp[index] != -1) return dp[index];
+
+        int rob = nums[index] + solve (index - 2, nums, dp);
+        int skip = solve (index - 1, nums, dp);
+
+        return dp[index] = max(rob, skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve (n -1, nums, dp);
+    }
+};
