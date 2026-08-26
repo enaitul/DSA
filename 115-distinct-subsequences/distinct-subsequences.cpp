2a46d0fce6d27a711cc77599a5aac26debae9b8c
Md Enaitul Hoque <127648815+enaitul@users.noreply.github.com>
2026-08-26 15:57:39 +0530

    Time: 31 ms (53.43%) | Memory: 44.3 MB (21.28%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int solve (int i , int j, vector<vector<int>> &dp, string &s, string &t){
-        if (j < 0) return 1;
-        if (i < 0) return 0;
-        
-        if (dp[i][j]!= -1) return dp[i][j];
-        if (s[i] == t[j]){
-            return dp[i][j] = solve(i-1, j-1, dp,s,t) + solve (i-1, j, dp, s, t);
-        }
-        return dp[i][j] = solve (i-1, j,dp,s,t);
-    }
     int numDistinct(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return solve (m-1, n-1, dp, s, t);
+        int n = s.size();
+        int m = t.size();
+        
+        // Use double to safely avoid integer overflow on large test cases
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
+
+        // Base case: An empty target string t (j = 0) can be formed in 1 way from any prefix of s
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    // Option 1: Match s[i-1] with t[j-1] -> dp[i-1][j-1]
+                    // Option 2: Skip s[i-1]              -> dp[i-1][j]
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    // Characters don't match, we must skip s[i-1]
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return (int)dp[n][m];
     }
 };
