0854eede3956ede16391b439a1b0127625a82227
Md Enaitul Hoque <127648815+enaitul@users.noreply.github.com>
2026-09-11 14:26:09 +0530

    Time: 3 ms (84.06%) | Memory: 28.7 MB (88.78%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        vector<int> freq (10, 0);
+        int ans = 0;
+        for (auto x: digits){
+            freq[x]++;
+        }
+        for (int i = 1; i<=9; i++){
+            for (int j = 0; j<= 9; j++){
+                for (int k = 0; k<= 8; k+=2){
+                    int need[10] = {0};
+                    
+                    need[i]++;
+                    need[j]++;
+                    need[k]++;
+                
+                bool possible = true;
+                for (int d = 0; d<= 9; d++){
+                    if (need[d] > freq[d]){
+                        possible = false;
+                        break;
+                    }
+                }
+                if (possible){
+                    ans++;
+                }
+                }
+            }
+        }
+        return ans;
+    }
+};
