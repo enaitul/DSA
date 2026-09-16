071bb8c96880c70677738325db3870e3fa7e0ef4
Md Enaitul Hoque <127648815+enaitul@users.noreply.github.com>
2026-09-16 19:31:53 +0530

    Time: 1781 ms (7.27%) | Memory: 9.5 MB (69.67%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool ispalindrome(int i, int j, string& s) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int solve(int i, int n, string& s, vector<int>& dp) {
+        if (i == n)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int mincost = INT_MAX;
+        for (int j = i; j < n; j++) {
+            
+            if (ispalindrome(i, j, s)) {
+                int cost = 1 + solve(j+1, n, s, dp);
+                mincost = min(mincost, cost);
+            }
+        }
+        return dp[i] = mincost;
+    }
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n, -1);
+        return solve(0, n, s, dp) - 1;
+    }
+};
